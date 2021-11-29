12c8992db62881dca15addd1c6c9e987c52b578a
NateAnderson500 <nja5387@gmail.com>
2021-11-29 14:13:56 -0500

    added python calls

--- FacialSoftware.sh
+++ FacialSoftware.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # FacialSoftware
 
-# Faical software for the Facial Recognition Complementor
+# Facial software for the Facial Recognition Complementor
 # Nate Anderson
-# Version Beta 0.13
+# Version Beta 0.5
 
 # Prints welcome message
 echo "Welcome to the Anderson Complemmentor (Patent Pending)"
+cd FacialSoftware/
 sleep 3
 
 # Prints instructions and takes picture
@@ -24,9 +25,8 @@ echo "2"
 echo "1"
 echo "Click! Picture taken!"
 
-#this code will eventually take the picture and pass it to the database and actual facial recognition software
-#ASKS DATABASE TO SEE IF THERE IS A MATCH
-#IF THERE IS A MATCH IT WILL PASS THE INDEX TO THE COMPLEMENTS CODE AND PRINT THE COMPLEMENT TO THE SCREEN
-#IF IT IS NOT A MATCH THEN IT WILL SEND THE FACE TO MY EMAIL AND ASK ME TO ASSIGN AN INDEX AND A COMPLEMENT.
+python3 -m venv FacialSoftware
+source FacialSoftware/bin/activate
+python app.py face.jpg
 
 echo "Thank you for useing the Facial Recognition Complementor! Have an amazing day!! :)"
