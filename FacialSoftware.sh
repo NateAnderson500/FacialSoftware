b1cf6e330dfcebf3806ab9343467f414b04dca65
NateAnderson500 <nja5387@gmail.com>
2021-11-29 14:21:15 -0500

    made the countdown better

--- FacialSoftware.sh
+++ FacialSoftware.sh
@@ -13,16 +13,26 @@ sleep 3
 # Prints instructions and takes picture
 echo "Please look at the camera for 10 seconds"
 echo "10"
+sleep 1
 echo "9"
+sleep 1
 echo "8"
+sleep 1
 echo "7"
+sleep 1
 echo "6"
+sleep 1
 echo "5"
+sleep 1
 raspistill -o face.jpg
 echo "4"
+sleep 1
 echo "3"
+sleep 1
 echo "2"
+sleep 1
 echo "1"
+sleep 1
 echo "Click! Picture taken!"
 
 python3 -m venv FacialSoftware
