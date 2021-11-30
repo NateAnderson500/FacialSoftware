2a40b9673aea9a49d498becac15bfeddaefd7a77
NateAnderson500 <nja5387@gmail.com>
2021-11-30 12:21:19 -0500

    testing file managment

--- FacialSoftware.sh
+++ FacialSoftware.sh
@@ -7,7 +7,7 @@
 
 # Prints welcome message
 echo "Welcome to the Anderson Complemmentor (Patent Pending)"
-cd FacialSoftware/
+# cd FacialSoftware/
 sleep 3
 
 # Prints instructions and takes picture
@@ -35,8 +35,8 @@ echo "1"
 sleep 1
 echo "Click! Picture taken!"
 
-python3 -m venv FacialSoftware
-source FacialSoftware/bin/activate
+# python3 -m venv FacialSoftware
+# source FacialSoftware/bin/activate
 python app.py face.jpg
 
 echo "Thank you for useing the Facial Recognition Complementor! Have an amazing day!! :)"
