7434b3af1a7ebf4c0e5c86c446ba8d37a5564325
NateAnderson500 <nja5387@gmail.com>
2021-12-01 13:43:31 -0500

    Added better comments

--- FacialSoftware.sh
+++ FacialSoftware.sh
@@ -3,14 +3,16 @@
 
 # Facial software for the Facial Recognition Complementor
 # Nate Anderson
-# Version Beta 0.7
+# Version Beta 0.9
 
 # Prints welcome message
 echo "Welcome to the Anderson Complemmentor (Patent Pending)"
 sleep 3
 
-# Prints instructions and takes picture
+# Prints instructions
 echo "Please look at the camera for 10 seconds"
+
+# Prints a countdown
 echo "10"
 sleep 1
 echo "9"
@@ -23,7 +25,7 @@ echo "6"
 sleep 1
 echo "5"
 sleep 1
-raspistill -o face.jpg
+raspistill -o face.jpg # Takes Picture
 echo "4"
 sleep 1
 echo "3"
@@ -37,11 +39,13 @@ echo "Click! Picture taken!"
 # Runs the facial recognition code
 python app.py face.jpg
 
+# Prints line space
 echo ""
 
 # Runs the complementor code
 javac Main.java
 java Main
 
+# Prints exit message
 echo ""
 echo "Thank you for useing the Facial Recognition Complementor! Have an amazing day!! :)"
